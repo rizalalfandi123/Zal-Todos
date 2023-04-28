e5beb3e6c0c0c169516562f5db6c351a15277822
Rizal Alfandi <rizalalfandi123456789@gmail.com>
2023-04-28 21:33:40 +0700

    add cypress configuration

--- src/pages/navigation-bar.page/index.tsx
+++ src/pages/navigation-bar.page/index.tsx
@@ -1,31 +1,24 @@
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
+import Drawer, { drawerClasses } from '@mui/material/Drawer';
 import Container from '@mui/material/Container';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
+
 import Toolbar from '@mui/material/Toolbar';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import Typography from '@mui/material/Typography';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
-import { SxProps, useTheme, Theme } from '@mui/material/styles';
-import { NavbarList } from '@components';
-import { navbarList } from './list.navigation-bar';
+import { Theme, SxProps, useTheme } from '@mui/material/styles';
 import { NavigationBarDrawer } from './drawer.navigation-bar';
 import { AppBar } from './app-bar.navigation-bar';
 
-const appBarStyle: SxProps<Theme> = (theme) => ({
- marginLeft: { sm: `${theme.additionalFields.sideBarWidth}px` },
- zIndex: (theme) => theme.zIndex.drawer + 1,
- height: theme.additionalFields.appBarHeight + 'px',
+const containerStyle: SxProps<Theme> = { display: 'flex' };
+
+const navStyle: SxProps<Theme> = (theme) => ({
+ width: { sm: theme.additionalFields.sideBarWidth },
+ flexShrink: { sm: 0 },
+});
+
+const tempDrawerStyle: SxProps<Theme> = (theme) => ({
+ display: { xs: 'block', sm: 'none' },
+ '& .MuiDrawer-paper': { boxSizing: 'border-box', width: theme.additionalFields.sideBarWidth },
 });
 
 export const NavigationBarPage = () => {
@@ -42,10 +35,10 @@ export const NavigationBarPage = () => {
  const handleDrawerToggle = () => setMobileOpen((prev) => !prev);
 
  return (
-  <Box sx={{ display: 'flex' }}>
+  <Box sx={containerStyle}>
    <AppBar handleDrawerToggle={handleDrawerToggle} />
 
-   <Box component='nav' sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }} aria-label='mailbox folders'>
+   <Box component='nav' sx={navStyle}>
     <Drawer
      variant='temporary'
      open={mobileOpen}
